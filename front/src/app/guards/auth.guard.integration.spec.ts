f383392903fe21014e4ddb3db24922ccb1f8068b
AlexisTrouwaert <alexis.trouwaert@gmail.com>
2025-08-16 11:19:10 +0200

    commit

--- front/src/app/guards/auth.guard.integration.spec.ts
+++ front/src/app/guards/auth.guard.integration.spec.ts
@@ -1,35 +1,54 @@
 import { AuthGuard } from './auth.guard';
 import { SessionService } from '../services/session.service';
-import { Router } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { expect } from '@jest/globals';
+import { SessionsModule } from '../features/sessions/sessions.module';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
 
 describe('AuthGuard (integration)', () => {
-  let guard: AuthGuard;
-  let sessionServiceMock: jest.Mocked<SessionService>;
-  let routerMock: jest.Mocked<Router>;
+
+  let router: Router;
+  let sessionService: SessionService;
+
+  @Component({ template: 'Page de session' })
+class MockSessionComponent {}
+
+@Component({ template: 'Page de login' })
+class MockLoginComponent {}
+
+  const routes: Routes = [
+    { path: 'sessions', component: MockSessionComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: MockLoginComponent }
+  ];
 
   beforeEach(() => {
-    sessionServiceMock = { isLogged: false } as any;
-    routerMock = { navigate: jest.fn() } as any;
-
-    guard = new AuthGuard(routerMock, sessionServiceMock);
+    TestBed.configureTestingModule({
+      declarations: [
+        MockSessionComponent, 
+        MockLoginComponent
+      ],
+      imports: [RouterTestingModule.withRoutes(routes)],
+      providers: [AuthGuard, SessionService]
+    });
+    router = TestBed.inject(Router);
+    sessionService = TestBed.inject(SessionService);
   });
 
-  it('should allow activation if user is logged in', () => {
-    sessionServiceMock.isLogged = true;
+  it('should allow activation if user is logged in', async () => {
+    sessionService.isLogged = true;
 
-    const result = guard.canActivate();
+    await router.navigate(['/sessions']);
 
-    expect(result).toBe(true);
-    expect(routerMock.navigate).not.toHaveBeenCalled();
+    expect(router.url).toBe('/sessions')
   });
 
-  it('should prevent activation and redirect if user is not logged in', () => {
-    sessionServiceMock.isLogged = false;
+  it('should prevent activation and redirect if user is not logged in', async () => {
+    sessionService.isLogged = false;
+    
+    await router.navigate(['/sessions']);
 
-    const result = guard.canActivate();
-
-    expect(result).toBe(false);
-    expect(routerMock.navigate).toHaveBeenCalledWith(['login']);
+    expect(router.url).toBe('/login');
   });
 });
